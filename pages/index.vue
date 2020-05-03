6ce2951f519f4209c14e3795c064c24b141804f2
kght6123 <freelance@kght6123.jp>
2020-05-03 19:20:24 +0900

    \/ \/

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,14 @@
       <div></div>
       <div><font-awesome-icon :icon="['fas', 'angle-up']" /></div>
       <div></div>
-      <div><font-awesome-icon :icon="['fas', 'angle-left']" /></div>
+      <div @click="goPrevDay"><font-awesome-icon :icon="['fas', 'angle-left']" /></div>
       <div class="p-2">
         <Breadcrumbs :items="[
-        { label: 'プロジェクト名', url: '#' },
-        { label: '6月', url: '#' },
-        { label: '第2週', url: '#' },
-        { label: '6/14(火)', url: '#' },
-        ]" class="ml-3" />
+            { label: projectLabel, url: '#' },
+            { label: monthLabel, url: '#' },
+            { label: weekLabel, url: '#' },
+            { label: dayLabel, url: '#' },
+          ]" class="ml-3" />
         <div class="board">
           <draggable
             id="first"
@@ -88,7 +88,7 @@
           </draggable>
         </div>
       </div>
-      <div><font-awesome-icon :icon="['fas', 'angle-right']" /></div>
+      <div @click="goNextDay"><font-awesome-icon :icon="['fas', 'angle-right']" /></div>
       <div></div>
       <div><font-awesome-icon :icon="['fas', 'angle-down']" /></div>
       <div></div>
@@ -109,7 +109,7 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
-import draggable from "vuedraggable"
+import draggable from 'vuedraggable'
 import Breadcrumbs from '~/components/Breadcrumbs.vue'
 import Balloons from '~/components/Balloons.vue'
 import BacklogDetail from '~/components/BacklogDetail.vue'
@@ -123,44 +123,108 @@ export default {
   },
   data() {
     return {
-      id: 6,
+      id: '20200614',
+      projectLabel: 'MiraiScrum',
+      date: new Date(),
       list: [
-        { name: "1-1-1 モーダルウィンドウのコンポーネントを調べる", id: 0, pointMitumori: 1 },
-        { name: "1-1-2 モーダルウィンドウのコンポーネントを作る", id: 1, pointMitumori: 2 },
-        { name: "1-2 見積もりポイントのアイコンをコンポーネント化する", id: 2, pointMitumori: 1 }
+        { name: '1-1-1 モーダルウィンドウのコンポーネントを調べる', id: 0, pointMitumori: 1 },
+        { name: '1-1-2 モーダルウィンドウのコンポーネントを作る', id: 1, pointMitumori: 2 },
+        { name: '1-2 見積もりポイントのアイコンをコンポーネント化する', id: 2, pointMitumori: 1 }
       ],
       list2: [
-        { name: "3 見積もりポイントのアイコンを作る", id: 3, pointMitumori: 1 },
-        { name: "2-2 mongo-connectorが起動しない問題を調べる", id: 4, pointMitumori: 1 }
+        { name: '3 見積もりポイントのアイコンを作る', id: 3, pointMitumori: 1 },
+        { name: '2-2 mongo-connectorが起動しない問題を調べる', id: 4, pointMitumori: 1 }
       ],
       list3: [
-        { name: "2-1 mongo-connectorのコンテナに入る方法を調べる", id: 5, pointMitumori: 1 },
-        { name: "4-1 elastic-searchで日本語の全文検索をできる方法を調べる", id: 6, pointMitumori: 1 }
+        { name: '2-1 mongo-connectorのコンテナに入る方法を調べる', id: 5, pointMitumori: 1 },
+        { name: '4-1 elastic-searchで日本語の全文検索をできる方法を調べる', id: 6, pointMitumori: 1 }
       ]
     }
+  },
+  computed: {
+    nowId: {
+      get: function () {
+        return this.getStringForDateFormat('YYYYMMDD', this.date)
+      }
+    },
+    monthLabel: {
+      get: function () {
+        return this.getStringForDateFormat('MM月', this.date)
+      }
+    },
+    weekLabel: {
+      get: function () {
+        return this.getStringForDateFormat('第WM/WD週', this.date)
+      }
+    },
+    dayLabel: {
+      get: function () {
+        return this.getStringForDateFormat('DD日(DW)', this.date)
+      }
+    }
+  },
+  mounted() {
+    this.$nextTick(function () {
+      // this.projectLabel = 'test'
+    })
   },
   methods: {
      ...mapActions('services/BacklogDetail', [
       'showDetail',
       'hideDetail'
     ]),
+    goPrevDay: function () {
+      // console.log(this.date)
+      this.date.setTime(this.date.getTime() - 1 * 24 * 60 * 60 * 1000)
+      // this.$set(this, 'date', this.date)
+      this.date = new Date(this.date)
+    },
+    goNextDay: function () {
+      // console.log(this.date)
+      this.date.setTime(this.date.getTime() + 1 * 24 * 60 * 60 * 1000)
+      // this.$set(this, 'date', this.date)
+      this.date = new Date(this.date)
+    },
+    getStringForDateFormat: function (date_format_str, _date) {
+      const date = new Date(_date)
+      const year_str = date.getFullYear()
+      const month_str = 1 + date.getMonth()
+      const day_str = date.getDate()
+      const hour_str = date.getHours()
+      const minute_str = date.getMinutes()
+      const second_str = date.getSeconds()
+      const dayOfWeek = date.getDay()
+      const dayOfWeekStr = ['日', '月', '火', '水', '木', '金', '土'][dayOfWeek]
+      let format_str = '' + date_format_str // 'YYYYMMDDhhmmss'
+      date.setDate(day_str - dayOfWeek + 1)
+      format_str = format_str.replace(/YYYY/g, year_str)
+      format_str = format_str.replace(/MM/g, month_str)
+      format_str = format_str.replace(/WM/g, date.getMonth() + 1)
+      format_str = format_str.replace(/DD/g, day_str)
+      format_str = format_str.replace(/WD/g, date.getDate())
+      format_str = format_str.replace(/hh/g, hour_str)
+      format_str = format_str.replace(/mm/g, minute_str)
+      format_str = format_str.replace(/ss/g, second_str)
+      format_str = format_str.replace(/DW/g, dayOfWeekStr)
+      return format_str
+    },
     add: function() {
-      this.list.push({ name: "Juan1 " + this.id, id: this.id++ })
+      this.list.push({ name: 'Juan1 ' + this.id, id: this.id++ })
     },
     replace: function() {
-      this.list = [{ name: "Edgard1", id: this.id++ }]
+      this.list = [{ name: 'Edgard1', id: this.id++ }]
     },
     add2: function() {
-      this.list2.push({ name: "Juan2 " + this.id, id: this.id++ })
+      this.list2.push({ name: 'Juan2 ' + this.id, id: this.id++ })
     },
     replace2: function() {
-      this.list2 = [{ name: "Edgard2", id: this.id++ }]
+      this.list2 = [{ name: 'Edgard2', id: this.id++ }]
     },
     add3: function() {
-      this.list3.push({ name: "Juan3 " + this.id, id: this.id++ })
+      this.list3.push({ name: 'Juan3 ' + this.id, id: this.id++ })
     },
     replace3: function() {
-      this.list3 = [{ name: "Edgard3", id: this.id++ }]
+      this.list3 = [{ name: 'Edgard3', id: this.id++ }]
     },
     async show () {
       // this.$modal.show('hello-world');
