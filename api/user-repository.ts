fcef081058a0fb55b18a73dea9903bc4182aa10c
kght6123 <freelance@kght6123.jp>
2020-06-08 23:29:33 +0900

    とりあえず、さんぷる追加

--- api/user-repository.ts
+++ api/user-repository.ts
@@ -1,12 +1,14 @@
 class User {		
-	private _userId: string = '';
-	private _userName: string = '';
-	private _created: Date = new Date();
-	constructor(userId: string, userName: string, created: Date) {
-		this._userId = userId;
-		this._userName = userName;
-		this._created = created;
+	private _userId: string = ''
+	private _userName: string = ''
+	private _created: Date = new Date()
+	constructor(userId: string, userName: string, created?: Date) {
+		this._userId = userId
+		this._userName = userName
+		if (created != null)
+			this._created = created
 	}
+	
 	get userId(): string {
 		return this._userId
 	}
@@ -74,4 +76,68 @@ class UserRepository implements IUserRepository {
 		throw new Error("Method not implemented.");
 		// ここにSQLなど実行する処理を描く
 	}
+}
+interface IUserCreatePresenter {
+	Complete(outputData: UserCreateOutputData): void;
+}
+class UserCreateViewModel {
+	private _userId: string = '';
+	private _createdDate: string = '';
+	constructor(userId: string, createdDate: string) {
+		this._userId = userId;
+		this._createdDate = createdDate;
+	}
+  get userId(): string {
+		return this._userId
+	}
+	set userId(userId: string) {
+		this._userId = userId
+	}
+	get createdDate(): string {
+		return this._createdDate
+	}
+	set createdDate(createdDate: string) {
+		this._createdDate = createdDate
+	}
+}
+class UserCreatePresenter implements IUserCreatePresenter {
+	Complete(outputData: UserCreateOutputData): UserCreateViewModel {
+		const viewModel: UserCreateViewModel = new UserCreateViewModel(outputData.userId, outputData.created.toISOString())
+		return viewModel
+	}
+}
+class SystemUserCreatePresenter implements IUserCreatePresenter {
+	Complete(outputData: UserCreateOutputData): UserCreateViewModel {
+		const viewModel: UserCreateViewModel = new UserCreateViewModel(outputData.userId, outputData.created.toUTCString())
+		return viewModel
+	}
+}
+class UserCreateInteractor implements IUserCreateUseCase {
+	private _userRepository: IUserRepository
+	private _presenter: IUserCreatePresenter
+	constructor(userRepository: IUserRepository, presenter: IUserCreatePresenter) {
+		this._userRepository = userRepository
+    this._presenter = presenter
+	}
+	Handle(inputData: UserCreateInputData): void {
+    const duplicateUser: User = this._userRepository.FindByUserName(inputData.userName)
+    if (duplicateUser != null) {
+      throw new Error("duplicated")
+    }
+    const user: User = new User('', inputData.userName)
+    this._userRepository.Save(user)
+    const outputData: UserCreateOutputData = new UserCreateOutputData(user.userId, new Date())
+    this._presenter.Complete(outputData)
+	}
+}
+class UserController {
+  private userCreateUseCase: IUserCreateUseCase
+
+  constructor(userCreateUseCase: IUserCreateUseCase) {
+    this.userCreateUseCase = userCreateUseCase;
+  }
+  public CreateUser(userName: string): void {
+    var inputData = new UserCreateInputData(userName)
+    this.userCreateUseCase.Handle(inputData)
+  }
 }
